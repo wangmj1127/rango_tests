48cc38fa6b66f84b1f464f573884e488320ae102
Gerardo Aragon-Camarasa <gerardo.aragoncamarasa@glasgow.ac.uk>
2016-11-30 13:46:29 +0000

    update run_tests

---
+++ config_file_nolive.py
@@ -0,0 +1,59 @@
+import os, subprocess, shutil, glob
+
+ch3 = {'Chapter3ViewTests.test_index_contains_hello_message': False,
+        'Chapter3ViewTests.test_about_contains_create_message': False}
+
+ch4 = {'Chapter4ViewTest.test_view_has_title': False,
+        'Chapter4ViewTest.test_index_using_template': False,
+        'Chapter4ViewTest.test_about_using_template': False,
+        'Chapter4ViewTest.test_rango_picture_displayed': False,
+        'Chapter4ViewTest.test_about_contain_image': False,
+        'Chapter4ViewTest.test_serving_static_files': False}
+
+ch5 = {'Chapter5ModelTests.test_create_a_new_category': False,
+        'Chapter5ModelTests.test_create_pages_for_categories': False,
+        'Chapter5ModelTests.test_population_script_changes': False}
+
+ch6 = {'Chapter6ModelTests.test_category_contains_slug_field': False,
+        'Chapter6ViewTests.test_index_context': False,
+        'Chapter6ViewTests.test_index_displays_five_most_liked_categories': False,
+        'Chapter6ViewTests.test_index_displays_no_categories_message': False,
+        'Chapter6ViewTests.test_index_displays_five_most_viewed_pages': False,
+        'Chapter6ViewTests.test_index_contains_link_to_categories': False,
+        'Chapter6ViewTests.test_category_context': False,
+        'Chapter6ViewTests.test_category_page_using_template': False,
+        'Chapter6ViewTests.test_category_page_displays_pages': False,
+        'Chapter6ViewTests.test_category_page_displays_empty_message': False,
+        'Chapter6ViewTests.test_category_page_displays_category_does_not_exist_message': False}
+
+ch7 = {'Chapter7ViewTests.test_index_contains_link_to_add_category': False,
+        'Chapter7ViewTests.test_add_category_form_is_displayed_correctly': False,
+        'Chapter7ViewTests.test_add_page_form_is_displayed_correctly': False,
+        'Chapter7ViewTests.test_access_category_that_does_not_exists': False,
+        'Chapter7ViewTests.test_link_to_add_page_only_appears_in_valid_categories': False,
+        'Chapter7ViewTests.test_category_contains_link_to_add_page': False}
+
+ch8 = {'Chapter8ViewTests.test_base_template_exists': False,
+        'Chapter8ViewTests.test_titles_displayed': False,
+        'Chapter8ViewTests.test_pages_using_templates': False,
+        'Chapter8ViewTests.test_url_reference_in_index_page_when_logged': False,
+        'Chapter8ViewTests.test_url_reference_in_index_page_when_not_logged': False,
+        'Chapter8ViewTests.test_link_to_index_in_base_template': False,
+        'Chapter8ViewTests.test_url_reference_in_category_page': False}
+
+ch9 = {'Chapter9ModelTests.test_user_profile_model': False,
+        'Chapter9ViewTests.test_registration_form_is_displayed_correctly': False,
+        'Chapter9ViewTests.test_login_form_is_displayed_correctly': False,
+        'Chapter9ViewTests.test_login_provides_error_message': False,
+        'Chapter9ViewTests.test_login_redirects_to_index': False,
+        'Chapter9ViewTests.test_upload_image': False}
+
+ch10 = {'Chapter10SessionTests.test_user_number_of_access_and_last_access_to_index': False,
+        'Chapter10ViewTests.test_index_shows_number_of_visits': False,
+        'Chapter10ViewTests.test_about_page_shows_number_of_visits': False,
+        'Chapter10ViewTests.test_visit_number_is_passed_via_context': False}
+
+dict_chs = {'chapter3': ch3, 'chapter4': ch4, 'chapter5': ch5, 'chapter6': ch6,
+            'chapter7': ch7, 'chapter8': ch8, 'chapter9': ch9, 'chapter10': ch10}
+
+# dict_chs = {'chapter5': ch5}
---
+++ run_tests.py
@@ -0,0 +1,176 @@
+# How to run:
+# python run_tests.py -u https://github.com/<repository url> -s <student name> -d <date of deadline as YYYY-MM-DD>
+
+__author__ = 'Gerardo Aragon, David Manlove'
+
+import os, subprocess, shutil
+import config_file_nolive as cf
+import errno, os, stat
+
+# TODO: e-mail results
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# From: http://stackoverflow.com/questions/1213706/what-user-do-python-scripts-run-as-in-windows
+def handleRemoveReadonly(func, path, exc):
+  excvalue = exc[1]
+  if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+      os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+      func(path)
+  else:
+      raise
+
+def generate_file_list(test_cases):
+    '''
+        Helper function to generate a file list of the test set
+    '''
+    # list_no = []
+    # print list_no
+    # for each in test_cases:
+    #     # print int(each.replace("chapter", ""))
+    #     list_no.append(int(each.replace("chapter", "")))
+
+    out = ['decorators.py', 'test_utils.py']
+    for ch in test_cases:
+        out.append('tests_' + ch + '.py')
+
+    return out
+
+def runtests(in_tests, in_errors):
+    '''
+    Function that runs tests given that a test has not passed
+    '''
+    out_tests = in_tests
+    out_errors = in_errors
+    for ch in in_tests:
+        for key in in_tests[ch]:
+            if in_tests[ch][key] is False:
+                temp_test = 'rango.tests_' + ch + '.' + key
+                print temp_test
+                process = subprocess.Popen(['python', 'manage.py', 'test', temp_test], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = process.communicate()
+                p_status = process.wait()
+                # print err
+                if 'errors' in err or 'Traceback' in err:
+                    print '++++++++++++++++++++++++++++ FAILED!!!!!! ++++++++++++++++++++++++++++'
+                    out_errors[key] = temp_test + '\n' + err
+                else:
+                    print '++++++++++++++++++++++++++++ PASSED!!!!!! ++++++++++++++++++++++++++++'
+                    out_tests[ch][key] = True
+                    out_errors[key] = None
+
+    return out_tests, out_errors
+
+def main(url_git, student_no, date_deadline):
+    '''
+    Main function that clones the repository given the deadline date, runs tests for each commit found in the repository
+    and saves results and errors in the 'output' folder for the given student number
+    '''
+
+    GIT_CLONE = 'git clone '
+    GIT_CHKOUT = 'git checkout'
+    GIT_CHKOUT_MASTER = 'git checkout master@{' + date_deadline + '23:59:59}'
+    GIT_ADD = 'git add .'
+    GIT_STASH = 'git stash'
+    GIT_STASH_DROP = 'git stash drop'
+    GIT_LOG = ['git', 'log', '--pretty=%H', '--before='+date_deadline]
+    TEMP_DIR = 'temporal'
+
+    test_cases = cf.dict_chs
+    test_files = generate_file_list(test_cases)
+    error_in_tests = dict()
+    try:
+        shutil.rmtree(os.path.join(BASE_DIR, TEMP_DIR), ignore_errors=False, onerror=handleRemoveReadonly)
+    except:
+        print os.path.join(BASE_DIR, TEMP_DIR) + " does not exist!!"
+
+    ## Clone Repository
+    ret = subprocess.call(GIT_CLONE + url_git + " " + TEMP_DIR, shell=True)
+    assert ret == 0
+
+    # Retrieve log history
+    os.chdir(os.path.join(BASE_DIR, TEMP_DIR))
+
+    process = subprocess.Popen(GIT_LOG, stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    commits = out.split('\n')[0:-1]
+    commits.reverse()
+
+    print "Repository has " + str(len(commits)) + " commits!"
+
+    working_dir = ''
+    for root, dirs, files in os.walk("."):
+        for name in files:
+            if 'urls.py' == name:
+                working_dir = os.path.dirname(os.path.abspath(root))
+                break
+
+    print "Project dir: " + working_dir
+    assert os.path.isdir(os.path.abspath(working_dir + '/rango'))
+
+    # Iterate over commits and run tests
+    for c in commits:
+        os.chdir(os.path.join(BASE_DIR, TEMP_DIR))
+        ret = subprocess.call(GIT_CHKOUT + " " + c, shell=True)
+        assert ret == 0
+
+        # RUN TESTS HERE!!!!
+        if os.path.isdir(os.path.abspath(working_dir + '/rango')):
+            os.chdir(working_dir)
+            try:
+                subprocess.call('python manage.py migrate')
+            except:
+                print "Error while migrating rango!"
+            for each in test_files:
+                shutil.copyfile(os.path.join(BASE_DIR, each), os.path.join(working_dir , 'rango', each))
+
+
+            test_cases, error_in_tests = runtests(test_cases, error_in_tests)
+        # ==================
+
+        # Discard everything
+        os.chdir(os.path.join(BASE_DIR, TEMP_DIR))
+        ret = subprocess.call(GIT_ADD, shell=True)
+        assert ret == 0
+        ret = subprocess.call(GIT_STASH, shell=True)
+        ret = subprocess.call(GIT_STASH_DROP, shell=True)
+        ret = subprocess.call(GIT_CHKOUT_MASTER, shell=True)
+        assert ret == 0
+    ## -------
+    os.chdir(BASE_DIR)
+
+    # copy reports to student directory
+    dir_student = os.path.join(BASE_DIR, "output", student_no)
+    if not os.path.exists(dir_student):
+        os.makedirs(dir_student)
+
+    import json
+    with open(os.path.join(dir_student, 'results.json'), 'w') as fp:
+        json.dump(test_cases, fp, indent=4)
+
+    with open(os.path.join(dir_student, 'report_errors.txt'), 'w') as fp:
+        for each in error_in_tests:
+            if error_in_tests[each] is not None:
+                fp.write(error_in_tests[each])
+                fp.write('===========================================================================\n\n\n')
+
+    # with open(os.path.join(dir_student, 'report_errors.txt'), 'w') as fp:
+    #     for each in error_in_tests:
+    #         fp.write(each)
+    #         fp.write('===========================================================================\n\n\n')
+
+    shutil.rmtree(os.path.join(BASE_DIR, TEMP_DIR), ignore_errors=False, onerror=handleRemoveReadonly)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="git repository where tests should run")
+    parser.add_argument("-s", "--student", help="student number")
+    parser.add_argument("-d", "--deadline", help="deadline")
+    args = parser.parse_args()
+
+    if args.url is not None and args.student is not None and args.deadline is not None:
+        main(args.url, args.student, args.deadline)
+    else:
+        raise BaseException("url, student number and deadline are required!!. Type 'python main_script.py -h' for further help")
